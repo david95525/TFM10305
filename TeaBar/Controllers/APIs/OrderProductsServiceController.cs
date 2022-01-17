f9d84cc7df28ceba1541e12b1227c7d8e421a774
david <mooneve95525@gmail.com>
2022-01-18 02:55:21 +0800

    商品頁面可新增商品到購物車 更新了 OrderProductsServiceController

--- TeaBar/Controllers/APIs/OrderProductsServiceController.cs
+++ TeaBar/Controllers/APIs/OrderProductsServiceController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TeaBar.Data;
+using TeaBar.Models;
 using TeaBar.Models.ViewModels;
 
 namespace TeaBar.Controllers.APIs
@@ -28,17 +29,60 @@ namespace TeaBar.Controllers.APIs
         [Consumes("application/json")]
         public IActionResult AddToCart([FromBody] OrderProductsViewModel data) //取得產品資料
         {
+            string userName = User.Identity.Name;//當前identity中使用者名稱
+            HttpContext.Session.SetString("username", userName);
+         
+            Products product = _dbcontext.Products.FirstOrDefault(p => p.ProductID == data.productID);
+            Categories categories = _dbcontext.Categories.FirstOrDefault(c => c.CategoryID == product.CategoryID);
+              //隨機決定discount
+            List <Discount> discounts = _dbcontext.Discount.ToList();
+            Random r = new Random();
+            int index= r.Next(0, 10);
+            Discount discount = discounts[index];
+            // 轉換成購物車viewmodel
+            List<CartViewModel> carts = new List<CartViewModel>();
+            CartViewModel cart= new CartViewModel()
+            {
+                UserID = userName,
+                ProductId = product.ProductID,
+                ProductName = product.ProductName,
+                UnitPrice = data.size.price,
+                Quantity = data.quentity,
+                DiscountId = discount.DiscountID,
+                Discount = discount.DiscountRule,
+                Sweetness = data.sweetness,
+                Ice = data.ice,
+                Ingredient = data.ingredient[0].name,
+                Picture = product.Picture,
+                Note = "無",
+                Size = data.size.name,
+                StoreID = categories.StoreID
+            };
+
+            //讀舊的
+            if (HttpContext.Session.Keys.Contains(userName))
+            {
+                string oldcart= HttpContext.Session.GetString(userName);
+                carts = Newtonsoft.Json.JsonConvert.DeserializeObject<List<CartViewModel>>(oldcart);
+                carts.Add(cart);
+            }
+            else
+            //在空的購物車填入
+            {
+                carts.Add(cart);
+            }
             try
             {
-                string jsonstring = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+                string jsonstring = Newtonsoft.Json.JsonConvert.SerializeObject(carts);
                 //存入session
-                HttpContext.Session.SetString("cartItem", jsonstring);
+                HttpContext.Session.SetString(userName, jsonstring);
 
             }
             catch (InvalidCastException e)
             {
                 Console.WriteLine(e);
             }
+        
             return Ok();
         }
 
