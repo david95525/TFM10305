5cf2f6f9eca3fafae24022d0f728f22458e18fc5
Loraine-Yan <u100034010@gmail.com>
2022-01-19 10:19:01 +0800

    處理branch

--- TeaBar/Controllers/APIs/CategoriesApiController.cs
+++ TeaBar/Controllers/APIs/CategoriesApiController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using TeaBar.Data;
 using TeaBar.Models;
+using TeaBar.Models.ViewModels;
 
 namespace TeaBar.Controllers.APIs
 {
@@ -31,27 +32,50 @@ namespace TeaBar.Controllers.APIs
             return result;
         }
 
-        //TODO
-        //[HttpGet]
-        //[Route("CategoryQuantity")]
-        //[Produces("application/json")]
-        //public int GetCategoryQuantity() //取得種類數量
-        //{            
-        //    var result1 = _context.OrderDetails
-        //    .Where(o => o.ProductID == 1).Count();
-        //    var result2 = _context.OrderDetails
-        //    .Where(o => o.ProductID == 2).Count();
-        //    var result3 = _context.OrderDetails
-        //    .Where(o => o.ProductID == 3).Count();
+        [HttpGet]
+        [Route("CategoryQuantity")]
+        [Produces("application/json")]       
+        public List<List<int>> GetCategoryQuantity()//取得種類數量
+        {
+            var ProductManageChartView
+                = (from od in _context.OrderDetails
+                   join p in _context.Products on od.ProductID equals p.ProductID
+                   join c in _context.Categories on p.CategoryID equals c.CategoryID
+                   select new ProductManageChartView
+                   {
+                       CategoryID = c.CategoryID,
+                       Note = od.Note,
 
-        //    //1.茶
-        //    //2.奶
-        //    //3.水果
-        //    return (result1, result2, result3);
+                   }).ToList();
 
-        //    var result[] = { result1, result2, result3 };
-        //    return result;
-        //}
+            List<List<int>> CategoryCount = new List<List<int>>();
+            List<int> TeaCount = new List<int>();
+            List<int> MilkCount = new List<int>();
+            List<int> FruitCount = new List<int>();
+            var resultT1 = ProductManageChartView.Where(c => c.CategoryID == 1 && c.Note.IndexOf("緯育") >= 0).Count();
+            var resultT2 = ProductManageChartView.Where(c => c.CategoryID == 1 && c.Note.IndexOf("台南") >= 0).Count();
+            var resultT3 = ProductManageChartView.Where(c => c.CategoryID == 1 && c.Note.IndexOf("台北") >= 0).Count();
+            var resultT4 = ProductManageChartView.Where(c => c.CategoryID == 1 && c.Note.IndexOf("台中") >= 0).Count();
+            var resultM1 = ProductManageChartView.Where(c => c.CategoryID == 2 && c.Note.IndexOf("緯育") >= 0).Count();
+            var resultM2 = ProductManageChartView.Where(c => c.CategoryID == 2 && c.Note.IndexOf("台南") >= 0).Count();
+            var resultM3 = ProductManageChartView.Where(c => c.CategoryID == 2 && c.Note.IndexOf("台北") >= 0).Count();
+            var resultM4 = ProductManageChartView.Where(c => c.CategoryID == 2 && c.Note.IndexOf("台中") >= 0).Count();
+            var resultF1 = ProductManageChartView.Where(c => c.CategoryID == 3 && c.Note.IndexOf("緯育") >= 0).Count();
+            var resultF2 = ProductManageChartView.Where(c => c.CategoryID == 3 && c.Note.IndexOf("台南") >= 0).Count();
+            var resultF3 = ProductManageChartView.Where(c => c.CategoryID == 3 && c.Note.IndexOf("台北") >= 0).Count();
+            var resultF4 = ProductManageChartView.Where(c => c.CategoryID == 3 && c.Note.IndexOf("台中") >= 0).Count();
+
+            TeaCount.Add(resultT1); TeaCount.Add(resultT2); TeaCount.Add(resultT3); TeaCount.Add(resultT4);
+            MilkCount.Add(resultM1); MilkCount.Add(resultM2); MilkCount.Add(resultM3); MilkCount.Add(resultM4);
+            FruitCount.Add(resultF1); FruitCount.Add(resultF2); FruitCount.Add(resultF3); FruitCount.Add(resultF4);
+            CategoryCount.Add(TeaCount); CategoryCount.Add(MilkCount); CategoryCount.Add(FruitCount);
+
+            //1.茶   data: [緯育,台南,台北,台中]
+            //2.奶   data: [緯育,台南,台北,台中]
+            //3.水果 data: [緯育,台南,台北,台中]
+            //return CategoryCount;
+            return CategoryCount;
+        }
 
         // GET: api/CategoriesApi
         [HttpGet]
